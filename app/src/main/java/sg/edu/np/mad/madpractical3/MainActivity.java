81e2429243091045702dfb9af6586b603fe4e215
Isaac <s10257760@connect.np.edu.sg>
2024-04-27 21:22:09 +0800

    Added logic for handling the buttons when clicked and added corresponding toast widget

--- app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
@@ -1,7 +1,10 @@
 package sg.edu.np.mad.madpractical3;
-
+import android.view.View;
 import android.os.Bundle;
-
+import android.widget.TextView;
+import android.widget.Button;
+import java.lang.String;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,10 +18,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.parent), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
+
+
         });
+        User user = new User("John Doe", "MAD Developer", 1, false);
+
+        TextView tvName = findViewById(R.id.tvName);
+
+        TextView tvDescription = findViewById(R.id.tvDescription);
+
+        Button buttonFollow = findViewById(R.id.btnFollow);
+
+        tvName.setText(user.name);
+
+        tvDescription.setText(user.description);
+
+        buttonFollow.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                // get the text from button
+                // logic to handle different text (follow/unfollow), follow -> unfollow vice versa
+                String text = buttonFollow.getText().toString();
+                if(text.equals("Follow")){
+                    buttonFollow.setText("Unfollow");
+                    Toast.makeText(getApplicationContext(),"Unfollowed",Toast.LENGTH_SHORT).show();
+                }
+                else if (text.equals("Unfollow")){
+                    buttonFollow.setText("Follow");
+                    Toast.makeText(getApplicationContext(),"Followed",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    /*Toast is a widget that display a message for a fixed duration
+      subclass of the java.lang.Object class, Takes a context class as its parameter,
+      it has two constants "LENGTH_LONG" (Show for a long duration) and "LENGTH_SHORT" (show for a short duration).
+
+      Public method make Text makes a standard text toast, .show() displays the toast widget
+    * */
+
+
+
     }
+
+
 }
